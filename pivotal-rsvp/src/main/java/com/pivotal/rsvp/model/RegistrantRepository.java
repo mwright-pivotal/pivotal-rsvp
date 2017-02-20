f0dba9927911f22427620d9e9deaabe9f8be1a74
Mike Wright <mwright@pivotal.io>
2017-02-20 08:30:29 -0500

    reorg

--- pivotal-rsvp/src/main/java/com/pivotal/rsvp/model/RegistrantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pivotal.rsvp.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrantRepository extends JpaRepository<Registrant, Long> {
-
-}
